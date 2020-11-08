c72d14cd7bb5fc78df063862e2352f715c7a9a4f
Sonaal P. Pradeep <kingoftheland28@gmail.com>
2020-11-08 16:29:15 +0530

    Added Login/Register Pages

--- flaskpage.py
+++ flaskpage.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'sonaalpathlaipradeep'
 
 posts = [
     {
@@ -25,6 +28,20 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title = "About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash("Account Created for : {}!".format(form.username.data), 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title = "Registration", form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = "Login", form = form)
 
 if __name__ == "__main__":
     app.run(debug = True)
